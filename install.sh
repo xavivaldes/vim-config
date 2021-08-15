7ca6c45c979176c403926f49cfcd6324c98f5304
Xavi Valdes <xavivaldes@gmail.xom>
2021-08-15 12:32:51 +0200

    fix if not exists .vim

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ mv $HOME/.vimrc $HOME/.vimrc.old
 cp ./.vimrc $HOME/
 
 wget https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
+mkdir $HOME/.vim
 mkdir $HOME/.vim/colors
 mv ./solarized.vim $HOME/.vim/colors/
 
